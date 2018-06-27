eba712bf27a5585c552c8779ff4875aad6c51071
Isabela Barbosa <isabelabl1609@gmail.com>
2018-06-27 11:39:37 -0300

    App react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="cabecalho">
+        <header>
+          <h1> AEAEAEAE </h1>
+          <h2 className="azul"> EOEOEOEO </h2>
+        </header>
+        <p> Scratch me there, elevator butt under the bed. Meowzer meow to be let out yet happily ignoring when being 
+          called so purr meowing chowing and wowing, yet intrigued by the shower allways wanting food
+        </p>
+        <p> head under blanket so no one can see i could pee on i had the energy, human give me attention meow hopped 
+          up on catnip,
+        </p>
+        <p> yet bite the neighbor's bratty kid run outside as soon as door open shake treat bag. Poop in litter 
+          box,
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
